d1238fbb9949dbfd1bd1bcf59faa1a4239844bb9
David Mariluch <davidmariluch@mail.weber.edu>
2017-04-13 05:57:04 +0000

    Added checking for date to be digit and in correct range

--- create_report.py
+++ create_report.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import sys
-# import datetime
+import sqlite3
+import datetime
+import time
 
+def isInt(v):
+    try:    i = int(v)
+    except:  return False
+    return True
 
 def convertBegDate(date):
     """
@@ -9,7 +15,14 @@ def convertBegDate(date):
     """
     # Bad input
     if len(date) != 8:
-        print("Incorrect input")
+        # print("Incorrect input")
+        exit(1)
+
+    # check that input params are digit and between 0 and 99991231
+    if(isInt(date)):
+        if(int(date) < 0 or int(date) > 99991231):
+            exit(1)
+    else:
         exit(1)
 
     date = date[:4] + '-' + date[4:6] + '-' + date[6:] + ' 00:00'
@@ -22,19 +35,35 @@ def convertEndDate(date):
     """
     # Bad input
     if len(date) != 8:
-        print("Incorrect input")
+        # print("Incorrect input")
+        exit(1)
+
+    # check that input params are digit and between 0 and 99991231
+    if(isInt(date)):
+        if(int(date) < 0 or int(date) > 99991231):
+            exit(1)
+    else:
         exit(1)
 
     date = date[:4] + '-' + date[4:6] + '-' + date[6:] + ' 23:59'
     return date
 
 
-def createRepo(beg, end):
+def createReport(begDate, endDate):
     """
     Create report of transactions
     """
-    # newBegDate = convertBegDate(begDate)
-    # newEndDate = convertEndDate(endDate)
+    # convert date to correct format and error check
+    newBegDate = convertBegDate(begDate)
+    newEndDate = convertEndDate(endDate)
+    # query DB
+
+    # check that query is not empty
+    # if empty exit(2) else continue
+
+    name = "company_trans_"+begDate+"_"+endDate+".dat"
+    myFile = open(name, 'w')
+    myFile.close()
 
 
 def main():
@@ -43,11 +72,8 @@ def main():
     """
     beg = sys.argv[1]
     end = sys.argv[2]
-    createRepo(beg, end)
-    name = "company_trans_"+beg+"_"+end+".dat"
-    myFile = open(name, 'w')
-    myFile.close()
-    exit(0)
+    createReport(beg, end)
+
 
 if __name__ == "__main__":
     # Call Main
